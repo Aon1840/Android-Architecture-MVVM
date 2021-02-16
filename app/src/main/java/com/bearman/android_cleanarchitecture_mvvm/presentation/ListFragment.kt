ea4b4b9c74a8d2744623f5ed91bbed13b18c0707
Attapon Peungsook <aonattapon1840.ap@gmail.com>
2021-02-16 23:30:38 +0700

    Add navigation

--- app/src/main/java/com/bearman/android_cleanarchitecture_mvvm/presentation/ListFragment.kt
+++ app/src/main/java/com/bearman/android_cleanarchitecture_mvvm/presentation/ListFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.bearman.android_cleanarchitecture_mvvm.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +20,17 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addNoteButton.setOnClickListener { goToNoteDetail() }
+    }
+
+    private fun goToNoteDetail(id: Long = 0L) {
+        val action = ListFragmentDirections.actionGoToNote(id)
+        Navigation.findNavController(noteListView).navigate(action)
     }
 
 }
--- app/src/main/java/com/bearman/android_cleanarchitecture_mvvm/presentation/NoteFragment.kt
+++ app/src/main/java/com/bearman/android_cleanarchitecture_mvvm/presentation/NoteFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 
 import com.bearman.android_cleanarchitecture_mvvm.R
+import kotlinx.android.synthetic.main.fragment_note.*
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +20,12 @@ class NoteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        checkButton.setOnClickListener { Navigation.findNavController(it).popBackStack() }
     }
 
 }
